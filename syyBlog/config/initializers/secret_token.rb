d3aa2495198ffc11413997ff8b3972d93c820cf3
Soyoung Yoon <syoon1@andrew.cmu.edu>
2013-09-18 20:26:13 -0400

    syyBlog
    Alpha

---
+++ syyBlog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SyyBlog::Application.config.secret_token = '1192ecc4df21c49ced1bce1fb2361a3de5b736fee3653263ee134e68d74674f38c7ae3f50b6a1b55543b5384e3199f8cb166546fee94a4aeee90f1619be478e4'
